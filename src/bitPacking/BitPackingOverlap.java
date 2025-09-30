6226ffaa0461daebe7ad9938097eac811fc4515c
DeChab-Antoine <antoine.de-chabannes-curton-la-palice@univ-tlse3.fr>
2025-09-30 15:54:15 +0200

    feat : readValue pour Overlap

--- src/bitPacking/BitPackingOverlap.java
+++ src/bitPacking/BitPackingOverlap.java
@@ -34,8 +34,19 @@ public class BitPackingOverlap extends BitPacking {
 
 	@Override
 	protected int readValue(int i) {
-		// TODO Auto-generated method stub
-		return 0;
+		long start = (long) i * (long) k; // numéro du bit où débuter à écrire
+		int indWord = (int) start >>> 5; // indice du mot (/32)
+		int borneInf = (int) start & 31; // nb bits déjà écris dans le mot courant (% 32)
+		int borneSup = borneInf + k;
+		
+	    if (borneSup <= 32) {
+	        return BitOps.readBits(tabWords[indWord], borneInf, k);
+	    } else {
+	    	int space = 32-borneInf;
+	        int low  = BitOps.readBits(tabWords[indWord], borneInf, space);
+	        int high = BitOps.readBits(tabWords[indWord + 1], 0, k-space);
+	        return low | (high << space);
+	    }
 	}
 	
 }
